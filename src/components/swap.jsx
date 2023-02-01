367e0f0508a4e9035814d50ba9f8ff10433cbcbc
cybereye-devs <cybereye_devs@proton.me>
2023-02-01 17:50:34 +0100

    noswap

--- src/components/swap.jsx
+++ src/components/swap.jsx
@@ -1,20 +1,20 @@
-import { Center } from "@chakra-ui/react";
-import { SwapWidget } from "@uniswap/widgets";
-const theme = {
-  primary: "#C3D3D0",
-  secondary: "#000",
-  container: "#fff",
-  module: "#de9c9a",
-  accent: "#962213",
-  outline: "#000",
-  dialog: "#111F21",
-  networkDefaultShadow: "#fff",
-  accentSoft: "#9ec5ea",
-};
+// import { Center } from "@chakra-ui/react";
+// import { SwapWidget } from "@uniswap/widgets";
+// const theme = {
+//   primary: "#C3D3D0",
+//   secondary: "#000",
+//   container: "#fff",
+//   module: "#de9c9a",
+//   accent: "#962213",
+//   outline: "#000",
+//   dialog: "#111F21",
+//   networkDefaultShadow: "#fff",
+//   accentSoft: "#9ec5ea",
+// };
 const Swap = () => {
   return (
     <Center position={"relative"} top={-120}>
-      <SwapWidget theme={theme} />
+      {/* <SwapWidget theme={theme} /> */}
     </Center>
   );
 };
